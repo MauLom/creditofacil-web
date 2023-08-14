f7fd063ce8bcdd3b7e8f7f24f61c7b0520ece104
Mauricio <mauricio.lombano@gmail.com>
2023-08-13 22:49:37 -0600

    Replaced env reference

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,16 +1,16 @@
 import { MongoClient } from 'mongodb'
 
-if (!process.env.MONGo_URI) {
+if (!process.env.MONGODB_URI) {
   throw new Error('Invalid environment variable: "MONGODB_URI"')
 }
 
-const uri = process.env.MONGO_URI
+const uri = process.env.MONGODB_URI
 const options = {}
 
 let client
 let clientPromise: Promise<MongoClient>
 
-if (!process.env.MONGO_URI) {
+if (!process.env.MONGODB_URI) {
   throw new Error('Please add your Mongo URI to .env.local')
 }
 
